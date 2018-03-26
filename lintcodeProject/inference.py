57f05aad15ee4418781fd9180bad5718039ac5af
tingwu-zhang <18510665908@163.com>
2018-03-26 18:29:07 +0800

    mnist

--- lintcodeProject/inference.py
+++ lintcodeProject/inference.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 INPUT_NODE = 784
 OUTPUT_NODE = 10
-LAYER1_NODE = 500
+LAYER1_NODE = 600
 
 def get_weight_variable(shape, regularizer):
     weights = tf.get_variable("weights", shape, initializer=tf.truncated_normal_initializer(stddev=0.1))
